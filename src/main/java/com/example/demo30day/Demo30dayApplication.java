97eaa7fb878325dd2f5aa3b6326779441dcf7e26
Jocelyn Lee <biheyeblue@gmail.com>
2021-05-01 21:54:26 +0800

    Initial commit

---
+++ src/main/java/com/example/demo30day/Demo30dayApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo30day;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Demo30dayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Demo30dayApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/demo30day/Demo30dayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.demo30day;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Demo30dayApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
